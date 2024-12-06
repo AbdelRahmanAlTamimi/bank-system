e66fa1013baecef7770e057f71f0c65ed5ee0320
Abdelrahman Altamimi <tamimi6030@gmail.com>
2024-12-06 15:06:29 +0300

    Main Screen

--- clsInputValidate.h
+++ clsInputValidate.h
@@ -1,30 +1,14 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include "clsDate.h"
 #include "clsString.h"
+#include "clsDate.h"
 
 class clsInputValidate
 {
 
 public:
-    static bool IsNumberBetween(short Number, short From, short To)
-    {
-        if(Number >= From && Number <= To)
-            return true;
-        else
-            return false;
-    }
-
     static bool IsNumberBetween(int Number, int From, int To)
-    {
-        if (Number >= From && Number <= To)
-            return true;
-        else
-            return false;
-    }
-
-    static bool IsNumberBetween(float Number, float From, float To)
     {
         if (Number >= From && Number <= To)
             return true;
@@ -42,15 +26,33 @@ public:
 
     static bool IsDateBetween(clsDate Date, clsDate From, clsDate To)
     {
-        if (
-            (clsDate::IsDate1BeforeDate2(Date, To) &&
-             clsDate::IsDate1AfterDate2(Date, From)) ||
-            (clsDate::IsDate1BeforeDate2(Date, From) &&
-             clsDate::IsDate1AfterDate2(Date, To))
-            )
+        // Date>=From && Date<=To
+        if ((clsDate::IsDate1AfterDate2(Date, From) || clsDate::IsDate1EqualDate2(Date, From)) &&
+            (clsDate::IsDate1BeforeDate2(Date, To) || clsDate::IsDate1EqualDate2(Date, To)))
+        {
             return true;
-        else
-            return false;
+        }
+
+        // Date>=To && Date<=From
+        if ((clsDate::IsDate1AfterDate2(Date, To) || clsDate::IsDate1EqualDate2(Date, To)) &&
+            (clsDate::IsDate1BeforeDate2(Date, From) || clsDate::IsDate1EqualDate2(Date, From)))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    static short ReadShortNumber(string ErrorMessage = "Invalid Number, Enter again\n")
+    {
+        short Number;
+        while (!(cin >> Number))
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << ErrorMessage;
+        }
+        return Number;
     }
 
     static int ReadIntNumber(string ErrorMessage = "Invalid Number, Enter again\n")
@@ -65,6 +67,18 @@ public:
         return Number;
     }
 
+    static short ReadShortNumberBetween(short From, short To, string ErrorMessage = "Number is not within range, Enter again:\n")
+    {
+        int Number = ReadShortNumber();
+
+        while (!IsNumberBetween(Number, From, To))
+        {
+            cout << ErrorMessage;
+            Number = ReadShortNumber();
+        }
+        return Number;
+    }
+
     static int ReadIntNumberBetween(int From, int To, string ErrorMessage = "Number is not within range, Enter again:\n")
     {
         int Number = ReadIntNumber();
@@ -73,6 +87,30 @@ public:
         {
             cout << ErrorMessage;
             Number = ReadIntNumber();
+        }
+        return Number;
+    }
+
+    static float ReadFloatNumber(string ErrorMessage = "Invalid Number, Enter again\n")
+    {
+        float Number;
+        while (!(cin >> Number))
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << ErrorMessage;
+        }
+        return Number;
+    }
+
+    static float ReadFloatNumberBetween(float From, float To, string ErrorMessage = "Number is not within range, Enter again:\n")
+    {
+        float Number = ReadFloatNumber();
+
+        while (!IsNumberBetween(Number, From, To))
+        {
+            cout << ErrorMessage;
+            Number = ReadFloatNumber();
         }
         return Number;
     }
@@ -112,17 +150,5 @@ public:
         // Usage of std::ws will extract allthe whitespace character
         getline(cin >> ws, S1);
         return S1;
-    }
-
-    static float ReadFloatNumber(string ErrorMessage = "Invalid Number, Enter again\n")
-    {
-        float Number;
-        while (!(cin >> Number))
-        {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << ErrorMessage;
-        }
-        return Number;
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,56 +1,11 @@
 #include <iostream>
-#include "clsBankClient.h"
-#include "clsInputValidate.h"
-#include <iomanip>
-#include "clsUtil.h"
-
-void PrintClientRecordBalanceLine(clsBankClient Client)
-{
-
-    cout << "| " << setw(15) << left << Client.GetAccountNumber();
-    cout << "| " << setw(40) << left << Client.FullName();
-    cout << "| " << setw(12) << left << Client.GetAccountBalance();
-}
-
-void ShowTotalBalances()
-{
-
-    vector<clsBankClient> vClients = clsBankClient::GetClientsList();
-
-    cout << "\n\t\t\t\t\tBalances List (" << vClients.size() << ") Client(s).";
-    cout << "\n_______________________________________________________";
-    cout << "_________________________________________\n"
-         << endl;
-
-    cout << "| " << left << setw(15) << "Accout Number";
-    cout << "| " << left << setw(40) << "Client Name";
-    cout << "| " << left << setw(12) << "Balance";
-    cout << "\n_______________________________________________________";
-    cout << "_________________________________________\n"
-         << endl;
-
-    double TotalBalances = clsBankClient::GetTotalBalances();
-
-    if (vClients.size() == 0)
-        cout << "\t\t\t\tNo Clients Available In the System!";
-    else
-
-        for (clsBankClient Client : vClients)
-        {
-            PrintClientRecordBalanceLine(Client);
-            cout << endl;
-        }
-
-    cout << "\n_______________________________________________________";
-    cout << "_________________________________________\n"
-         << endl;
-    cout << "\t\t\t\t\t   Total Balances = " << TotalBalances << endl;
-    cout << "\t\t\t\t\t   ( " << clsUtil::NumberToText(TotalBalances) << ")";
-}
+#include "clsMainScreen.h"
 
 int main()
-
 {
-    ShowTotalBalances();
+    clsMainScreen::ShowMainMenue();
+
+    std::cout << "Press Enter to continue...";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     return 0;
 }
