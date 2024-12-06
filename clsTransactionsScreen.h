04a39e713c3fdfd99befbc876d96b2fb1c852c3b
Abdelrahman Altamimi <tamimi6030@gmail.com>
2024-12-06 17:21:48 +0300

    add total balances screen and funcitons

--- clsTransactionsScreen.h
+++ clsTransactionsScreen.h
@@ -5,6 +5,7 @@
 #include <iomanip>
 #include "clsDepositScreen.h"
 #include "clsWithdrawScreen.h"
+#include "clsTotalBalancesScreen.h"
 
 using namespace std;
 
@@ -41,7 +42,8 @@ private:
 
     static void _ShowTotalBalancesScreen()
     {
-        cout << "\n Balances Screen will be here.\n";
+        // cout << "\n Balances Screen will be here.\n";
+        clsTotalBalancesScreen::ShowTotalBalances();
     }
 
     static void _GoBackToTransactionsMenue()
