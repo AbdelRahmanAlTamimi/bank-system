ee5a48a6492a9088eb814331598a52dc0d759170
Abdelrahman Altamimi <tamimi6030@gmail.com>
2024-12-06 15:19:53 +0300

    List Clients Screen

--- clsMainScreen.h
+++ clsMainScreen.h
@@ -3,7 +3,7 @@
 #include "clsScreen.h"
 #include "clsInputValidate.h"
 #include <iomanip>
-
+#include "clsClientListScreen.h"
 
 using namespace std;
 
@@ -36,12 +36,11 @@ class clsMainScreen:protected clsScreen
             ShowMainMenue();
         }
 
-       static void _ShowAllClientsScreen()
-       {
-        cout << "\nClient List Screen Will be here...\n";
-        
-
-       }
+        static void _ShowAllClientsScreen()
+        {
+            //  cout << "\nClient List Screen Will be here...\n";
+            clsClientListScreen::ShowClientsList();
+        }
 
        static void _ShowAddNewClientsScreen()
        {
