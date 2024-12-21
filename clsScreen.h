3bf98adb4e73f171875a6a512c2d69620e5571ed
Abdelrahman Altamimi <tamimi6030@gmail.com>
2024-12-21 16:56:54 +0300

    add date and logged in username to all screens

--- clsScreen.h
+++ clsScreen.h
@@ -2,7 +2,7 @@
 #include <iostream>
 #include "clsUser.h"
 #include "Global.h"
-
+#include "clsDate.h"
 
 using namespace std;
 
@@ -18,7 +18,11 @@ protected :
             cout << "\n\t\t\t\t\t  " << SubTitle;
         }
         cout << "\n\t\t\t\t\t______________________________________\n\n";
+        cout << "\n\t\t\t\t\tUser: " << CurrentUser.GetUserName() << "\n";
+        cout << "\t\t\t\t\tDate: " << clsDate::DateToString(clsDate())
+             << "\n\n";
     }
+
     static bool CheckAccessRights(clsUser::enPermissions Permission)
     {
 
